c91d49f9cba88b88bd68e6ad8a821d7ac4cf8349
Gowtham-bit <gowthamsivakumar001@gmail.com>
2026-03-03 09:49:16 +0530

    Next Commit

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use((err, req, res, next) => {
 });
 
 app.use(cors({
-    origin: ['https://expenseiq-three.vercel.app', 'https://expenseiq-sgm.vercel.app', 'http://localhost:5173'], // Allowed frontend domains
+    origin: ['https://expenseiq-three.vercel.app', 'https://expenseiq-sgm.vercel.app', 'http://localhost:5173', 'http://localhost:8080'], // Allowed frontend domains
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
